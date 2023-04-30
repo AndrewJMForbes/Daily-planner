b5636d08fa43a8c420bf9ca3911fa49446daebc3
Forbes85 <vudo8585@gmail.com>
2023-04-30 16:25:52 -0500

    added bootstrap attributes to page

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+setInterval(function() {
+  $('#currentDay').text(dayjs().format('MMM DD, YYYY hh:mm:ssA'))
+})
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,6 +11,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +24,9 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+})
+$('#save').on('click', function(event) {
+  event.preventDefault;
+  console.log('save')
+
 });
